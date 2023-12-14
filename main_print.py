15288c9628d26655a4c9904e21db7fd0b94448d1
Grifcc <grifcc@foxmail.com>
2023-12-14 16:32:19 +0800

    rename

---
+++ main_print.py
@@ -0,0 +1,18 @@
+from modules.json_source import JsonSource
+from modules.spatial_filter import SpatialFilter
+from modules.time_filter import TimeFilter
+from modules.print_sink import PrintSink
+from modules.esti_position import EstiPosition
+
+from framework.pipeline import Pipeline   
+
+if __name__ == "__main__":
+    modules=[]
+    modules.append(JsonSource("simulated_data/simulate_data.json"))
+    modules.append(TimeFilter(1000))
+    modules.append(EstiPosition("data/JiulongLake.obj"))
+    modules.append(SpatialFilter(1000, distance_threshold=10.,max_map=10))
+    modules.append(PrintSink())
+
+    pipe= Pipeline(modules)
+    pipe.run()
