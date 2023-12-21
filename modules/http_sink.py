5b6472c5a00c327568cd926c4b98c4b2c8195969
Grifcc <grifcc@foxmail.com>
2023-12-21 13:05:53 +0800

    add http header

--- modules/http_sink.py
+++ modules/http_sink.py
@@ -2,28 +2,30 @@ from framework import Package
 from framework import Sink
 import requests
 import time
-
+import json
 
 class HttpSink(Sink):
     def __init__(self, url, max_retries=5):
         super().__init__("http_sink")
         self.url = url
         self.max_retries = max_retries
+        self.header = {"content-type": "application/json"}
 
     def close():
         # 对齐操作
         pass
-    
+
     def process(self, data: Package):
         retry_count = 0
         send_data = {}
-        send_data["time"] = data.time
+        send_data["timestamp"] = data.time
         send_data["obj_cnt"] = 1
-        send_data["objs"] = [{"id": data.global_id, "cls": data.cls,
+        send_data["objs"] = [{"id": data.global_id, "cls": data.class_id,
                               "gis": data.location, "obj_img": data.obj_img}]
-
+        send_data = json.dumps(send_data)
         while retry_count < self.max_retries:
-            response = requests.post(self.url, data=send_data)
+            response = requests.post(
+                self.url, data=send_data, headers=self.header)
             if response.status_code == 200 and eval(response.text)["resCode"] == 1:
                 return
             else:
