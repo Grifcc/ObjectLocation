e76c15f4f4ef80792c4b361c6e1b3bfe4dea04ab
Zhang Shuaihao <76192104+carlosxlll@users.noreply.github.com>
2023-12-26 12:16:52 +0800

    Camera params test (#41)
    * add a method named get_obj_point_from_uav_obj_point
    
    * 优化求相机位置

--- modules/esti_position.py
+++ modules/esti_position.py
@@ -2,9 +2,10 @@ from framework import Package
 from framework import Location
 import mesh_raycast
 from tools import get_ray, read_mesh, set_K, set_distortion_coeffs, set_camera_pose, UWConvert
-
+import numpy as np
 
 class EstiPosition(Location):
+    # enable  true:根据相机位姿定位目标点 false:根据道通uav_obj_pose重定位目标点
     def __init__(self, mesh_path=None, default_height=60, order="rzyx", enable=True, max_queue_length=None):
         super().__init__("EstiPosition", max_queue_length)
         self.mesh = read_mesh(mesh_path)  # mesh地图
@@ -30,7 +31,24 @@ class EstiPosition(Location):
         else:
             return min(result, key=lambda x: x['distance'])[
                 'point']
+        
+    def get_point_form_uav_object_point(self, data: Package): 
+        p_camera = np.array(data.camera_pose[3:]).reshape(3,1)
+        p_obj = np.array([data.uav_utm]).reshape(3,1)
+        ray = p_obj - p_camera
+        ray = ray / np.linalg.norm(ray)
+        result = mesh_raycast.raycast(
+            p_camera.flatten(), ray, self.mesh)
+        if len(result) == 0:  # TODO
+            l = (self.default_height - t) / -ray[2]
+            # 计算交点坐标
+            inter_point = t - l * ray.reshape(3, 1)
+            return inter_point.flatten().tolist()
+        else:
+            return min(result, key=lambda x: x['distance'])[
+                'point']
+
 
     def process(self, data: Package):
         data.location = self.get_point(
-            data) if self.enable else data.uav_utm[:]
+            data) if self.enable else self.get_point_form_uav_object_point(data)
